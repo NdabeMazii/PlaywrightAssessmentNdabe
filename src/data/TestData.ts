49f0f5df33e46b655b3ea1d71ed0c1333c87d3f2
NdabeMazii <ndabezinhlemazibuko6@gmail.com>
2026-06-30 19:36:47 +0200

    Added TestData

---
+++ src/data/TestData.ts
@@ -0,0 +1,23 @@
+export const validUsers = {
+    admin: {
+        username: 'admin@gmail.com',
+        password: '@12345678',
+        // name: 'Nkosi'
+        gitUsername: 'git123',
+    },
+
+    classUser: {
+        username: 'ndabemazii@gmail.com',
+        password: 'Mazii@053103',
+        // name: 'Ndabezinhle'
+        gitUsername: 'git456',
+    },
+};
+
+export const invalidUsers = {
+    invalidEmail: {
+        username: 'thuthumazii',
+        password: 'Mazii@053103',
+        // name: 'Thuthuka'
+    },
+}; 
